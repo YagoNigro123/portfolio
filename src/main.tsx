b9061310d5e22400c1f4da910e4af76a5f5307a8
YagoGNC <yago.gn007@gmail.com>
2025-11-27 19:05:53 +0000

    refactor: clean up App structure and improve indentation

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import NavBar from '../components/layout/Navbar.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <NavBar/>
     <App />
   </StrictMode>,
 )
