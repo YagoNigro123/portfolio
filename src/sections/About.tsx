458a1466149205e01419796745afc67549ba7df5
YagoNigro123 <yago.gn007@gmail.com>
2025-11-30 02:23:15 +0000

    Update About section layout and animated background

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -10,15 +10,14 @@ const [tilt, setTilt] = useState({ rx: 0, ry: 0 });
 
   const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
     const rect = e.currentTarget.getBoundingClientRect();
-    const x = e.clientX - rect.left;  // posición dentro del div
+    const x = e.clientX - rect.left;
     const y = e.clientY - rect.top;
 
     const midX = rect.width / 2;
     const midY = rect.height / 2;
 
-    // Ajustá 10 si querés más/menos inclinación
-    const rotateY = ((x - midX) / midX) * 10;    // izquierda/derecha
-    const rotateX = -((y - midY) / midY) * 10;   // arriba/abajo
+    const rotateY = ((x - midX) / midX) * 10;
+    const rotateX = -((y - midY) / midY) * 10;
 
     setTilt({ rx: rotateX, ry: rotateY });
   };
@@ -27,9 +26,17 @@ const [tilt, setTilt] = useState({ rx: 0, ry: 0 });
     setTilt({ rx: 0, ry: 0 });
   };
   return (
-    <section className="about container" id="about">
+    <section className="about" id="about">
       <header className="about-header">
-        <div className="about-text">
+          <div
+            className="about-photo-tilt-wrapper"
+            onMouseMove={handleMouseMove}
+            onMouseLeave={handleMouseLeave}
+          >
+              <figure className="about-photo"style={{transform: `rotateX(${tilt.rx}deg) rotateY(${tilt.ry}deg)`,}}>
+              <img src='/photo.png' alt="Yo, en formato cachorro filosófico" className='photo'/>
+              </figure>
+          </div>
           <p className="title">About me</p>
           <h1 className="about-title">Code & Craft</h1>
           <p className="about-description">
@@ -37,16 +44,6 @@ const [tilt, setTilt] = useState({ rx: 0, ry: 0 });
             Disfruto diseñar soluciones simples pero inteligentes, con atención al detalle y un enfoque práctico.<br/>
             Me motiva construir proyectos que representen calidad, buen código y una experiencia fluida para el usuario.
           </p>
-        </div>
-        <div
-          className="about-photo-tilt-wrapper"
-          onMouseMove={handleMouseMove}
-          onMouseLeave={handleMouseLeave}
-        >
-            <figure className="about-photo"style={{transform: `rotateX(${tilt.rx}deg) rotateY(${tilt.ry}deg)`,}}>
-            <img src='/photo.png' alt="Yo, en formato cachorro filosófico" className='photo'/>
-            </figure>
-        </div>
       </header>
     </section>
   );
